d0a1e70d8eeba5e880be3f104a37d8d66fdc2442
Alejandro Bovet <ale.j1310@outlook.com>
2022-08-15 22:59:11 -0400

    V4

---
+++ BuscadorMangas/db_buscador.js
@@ -0,0 +1,83 @@
+const Mangas = [
+    {
+        titulo: 'BNHA',
+        tomos: 36,
+        año: 2012,
+        demografia: 'shonen',
+        autor: 'horikoshi'
+    },
+
+    {
+        titulo: 'naruto',
+        tomos: 136,
+        año: 1998,
+        demografia: 'seinen',
+        autor: 'kishi'
+    },
+
+    {
+        titulo: 'BC',
+        tomos: 30,
+        año: 2013,
+        demografia: 'spokon',
+        autor: 'yuki'
+    },
+
+    {
+        titulo: 'Dragon ball',
+        tomos: 58,
+        año: 1985,
+        demografia: 'shonen',
+        autor: 'akir'
+    },
+
+    {
+        titulo: 'slam dun',
+        tomos: 25,
+        año: 1992,
+        demografia: 'spokon',
+        autor: 'takehiko'
+    },
+
+    {
+        titulo: 'two piece',
+        tomos: 200,
+        año: 2000,
+        demografia: 'shonen',
+        autor: 'oba'
+    },
+
+    {
+        titulo: 'hxh',
+        tomos: 38,
+        año: 1997,
+        demografia: 'shonen',
+        autor: 'togash'
+    },
+
+    {
+        titulo: 'one kick',
+        tomos: 22,
+        año: 2010,
+        demografia: 'seinen',
+        autor: 'two'
+    },
+
+    {
+        titulo: 'bleak',
+        tomos: 57,
+        año: 2002,
+        demografia: 'romcom',
+        autor: 'tito'
+    },
+
+    {
+        titulo: 'demons sla',
+        tomos: 37,
+        año: 2014,
+        demografia: 'slicelife',
+        autor: 'goutuke'
+    },
+
+
+]
